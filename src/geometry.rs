e63189925c69ca2e9c0b755206a1ef392291cd88
qiubinglin <592559150@qq.com>
2020-09-05 17:31:10 +0800

    Vec3D ok

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,7 +1,29 @@
 use std::ops::{Add, Sub, Mul, Div, Index};
+use std::f64::consts;
 
+#[derive(Copy, Clone)]
 pub struct Vec3D {
     arr: [f64; 3],
+}
+
+impl Vec3D {
+    #[allow(dead_code)]
+    fn norm2(self) -> f64 {
+        self*self
+    }
+
+    #[allow(dead_code)]
+    fn norm(self) -> f64 {
+        f64::sqrt(self.norm2())
+    }
+}
+
+impl Index<usize> for Vec3D {
+    type Output = f64;
+
+    fn index(&self, rhs: usize) -> &f64 {
+        &self.arr[rhs]
+    }
 }
 
 impl Add for Vec3D {
@@ -32,6 +54,22 @@ impl Mul<f64> for Vec3D {
     type Output = Vec3D;
 
     fn mul(self, rhs: f64) -> Vec3D {
-        self
+        Vec3D { arr: [self.arr[0]*rhs, self.arr[1]*rhs, self.arr[2]*rhs] }
+    }
+}
+
+impl Mul<Vec3D> for f64 {
+    type Output = Vec3D;
+
+    fn mul(self, rhs: Vec3D) -> Vec3D {
+        Vec3D { arr: [self*rhs.arr[0], self*rhs.arr[1], self*rhs.arr[2]] }
+    }
+}
+
+impl Div<f64> for Vec3D {
+    type Output = Vec3D;
+
+    fn div(self, rhs: f64) -> Vec3D {
+        Vec3D { arr: [self.arr[0]/rhs, self.arr[1]/rhs, self.arr[2]/rhs] }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 include!("geometry.rs");
 
 fn main() {
-    println!("Hello, world!");
+    let a: Vec3D = Vec3D {
+        arr: [1.0, 0.0, 0.0]
+    };
+    let _b: Vec3D = a * 2.0;
+    let _c: f64 = a * _b;
+    println!("{}", _c)
 }
